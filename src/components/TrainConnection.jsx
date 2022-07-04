d5fd14dff2775bfe824bbaa25543d0db929b8dfd
BernardEpp <bernard.epp@acommo.com>
2022-07-04 20:30:44 +0200

    added component for connections

---
+++ src/components/TrainConnection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TrainConnection = (cities) => {
+    return (
+        <div className="TrainConnection">
+          {cities}
+        </div>
+    );
+};
+
+export default TrainConnection;
