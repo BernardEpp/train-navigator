d4f95892f8ea0160c334aca8c77cdedf2b469009
BernardEpp <bernard.epp@acommo.com>
2022-07-04 20:30:58 +0200

    implemented logic to call train search

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Component, useState } from 'react';
+import './App.css';
+import TrainConnection from './components/TrainConnection';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [connections, setConnections] = useState([]);
+  const [start, setStart] = useState("");
+  const [end, setEnd] = useState("");
+
+
+  const searchConnection = async (start, end) => {
+    console.log('search for start: ' + start + 'and end: ' + end);
+    const response = await fetch(
+      'http://localhost:8080?' + new URLSearchParams({start, end}) // start=Stuttgart&end=Kassel
+    );
+    const data = await response.json();
+    setConnections('Start' + start + 'End' + end);
+  };
 
   return (
-    <div className="App">
-     
-    </div>
-  )
-}
+    <main>
+      <h1>Train Navigator</h1>
+      <input type="text" placeholder='Start' onChange={(e) => setStart(e.target.value)} />
+      <input type="text" placeholder='End' onChange={(e) => setEnd(e.target.value)}/>
+      <button onClick={() => searchConnection(start, end)}>Search</button>
+
+      <ul>
+        {connections}
+        {/* {connections.map((cities) => (
+          <TrainConnection cities={cities}></TrainConnection>
+        ))} */}
+
+        {connections.length === 0 && 'No connections found.'}
+      </ul>
+    </main>
+  );
+};
 
 export default App
