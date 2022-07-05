73d5d8c1831916f96951e3bdb5e9108f19298159
Epp Bernard (ETAS/EES-SQ) <Bernard.Epp@etas.com>
2022-07-05 11:15:28 +0200

    removed commented out code

--- src/components/TrainConnection.jsx
+++ src/components/TrainConnection.jsx
@@ -2,10 +2,6 @@ import React from "react";
 
 const TrainConnection = ({ cities }) => {
   return (
-    // <div>
-    //   <div>Hello</div>
-    //   <div>{JSON.stringify(cities)}</div>
-    // </div>
     <li className="TrainConnection">
       {cities.map((city, index) => (
         <div key={index}>{city}</div>
