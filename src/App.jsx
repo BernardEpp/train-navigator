56bc30ecd852fdf6eecd52d93239f1ed600a43a5
Epp Bernard (ETAS/EES-SQ) <Bernard.Epp@etas.com>
2022-07-05 08:44:27 +0200

    fixed error in api call
    changed name of http get parameter

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
-import { Component, useState } from 'react';
-import './App.css';
-import TrainConnection from './components/TrainConnection';
-
+import { Component, useState } from "react";
+import "./App.css";
+import TrainConnection from "./components/TrainConnection";
 
 function App() {
   const [connections, setConnections] = useState([]);
   const [start, setStart] = useState("");
   const [end, setEnd] = useState("");
 
+  const searchConnection = async (start, dest) => {
+    console.log("search for start: " + start + " and end: " + dest);
 
-  const searchConnection = async (start, end) => {
-    console.log('search for start: ' + start + 'and end: ' + end);
     const response = await fetch(
-      'http://localhost:8080?' + new URLSearchParams({start, end}) // start=Stuttgart&end=Kassel
+      "http://localhost:8080?" + new URLSearchParams({ start, dest }) // start=Stuttgart&dest=Kassel
     );
     const data = await response.json();
-    setConnections('Start' + start + 'End' + end);
+    setConnections(data);
   };
 
   return (
     <main>
       <h1>Train Navigator</h1>
-      <input type="text" placeholder='Start' onChange={(e) => setStart(e.target.value)} />
-      <input type="text" placeholder='End' onChange={(e) => setEnd(e.target.value)}/>
+      <input
+        type="text"
+        placeholder="Start"
+        onChange={(e) => setStart(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="End"
+        onChange={(e) => setEnd(e.target.value)}
+      />
       <button onClick={() => searchConnection(start, end)}>Search</button>
 
       <ul>
@@ -31,10 +38,10 @@ function App() {
           <TrainConnection cities={cities}></TrainConnection>
         ))} */}
 
-        {connections.length === 0 && 'No connections found.'}
+        {connections.length === 0 && "No connections found."}
       </ul>
     </main>
   );
-};
+}
 
-export default App
+export default App;
